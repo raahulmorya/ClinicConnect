84bd51995df850ae75098f8c8609b7f4c623f9f1
Rahul Morya <raahulmorya@gmail.com>
2024-08-09 13:36:17 +0530

    Combined Server and Client

--- client/src/components/PatientList.js
+++ client/src/components/PatientList.js
@@ -23,31 +23,34 @@ const PatientList = () => {
       <h1>Patient List</h1>
       {error && <p>Error: {error}</p>}
       <ul>
-        {patients.map((patient) => (
-          <li className="small-padding-top" key={patient._id}>
-            <p>
-              <strong>ID:</strong> {patient.patientId}
-            </p>
-            <strong>{patient.name}</strong> - {patient.age} years old
-            <p>
-              <strong>Gender: </strong> {patient.gender}
-            </p>
-            <p>
-              <strong>Address: </strong> {patient.address}
-            </p>
-            <p>
-              <strong>Symptoms: </strong> {patient.symptoms}
-            </p>
-            <p>
-              <strong>Prescriptions: </strong>
-              <ul>
-                {patient.prescriptions.map((prescription, index) => (
-                  <ul key={index}>{prescription}</ul>
-                ))}
-              </ul>
-            </p>
-          </li>
-        ))}
+        {patients
+          .slice()
+          .reverse()
+          .map((patient) => (
+            <li className="small-padding-top" key={patient._id}>
+              <p>
+                <strong>ID:</strong> {patient.patientId}
+              </p>
+              <strong>{patient.name}</strong> - {patient.age} years old
+              <p>
+                <strong>Gender: </strong> {patient.gender}
+              </p>
+              <p>
+                <strong>Address: </strong> {patient.address}
+              </p>
+              <p>
+                <strong>Symptoms: </strong> {patient.symptoms}
+              </p>
+              <p>
+                <strong>Prescriptions: </strong>
+                <ul>
+                  {patient.prescriptions.map((prescription, index) => (
+                    <ul key={index}>{prescription}</ul>
+                  ))}
+                </ul>
+              </p>
+            </li>
+          ))}
       </ul>
     </div>
   );
