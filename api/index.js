1bc79fae46c1d959c093881be5b07aff14307c81
Rahul Morya <121626369+raahulmorya@users.noreply.github.com>
2024-08-09 20:55:04 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,12 +3,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const bcrypt = require('bcryptjs'); // Add bcryptjs for password hashing
+const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 
-
 const app = express();
-const port = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors());
@@ -28,7 +26,6 @@ const userSchema = new mongoose.Schema({
 });
 const User = mongoose.model("User", userSchema);
 
-
 // Patient Model
 const patientSchema = new mongoose.Schema({
   name: String,
@@ -36,12 +33,10 @@ const patientSchema = new mongoose.Schema({
   gender: String,
   address: String,
   symptoms: String,
-  prescriptions: [{ type: String }], // Array of prescriptions
+  prescriptions: [{ type: String }],
   patientId: { type: String, unique: true, default: () => `PAT-${Date.now()}` },
 });
-
 const Patient = mongoose.model('Patient', patientSchema);
-
 
 // Middleware to verify token
 const authenticateToken = (req, res, next) => {
@@ -52,7 +47,7 @@ const authenticateToken = (req, res, next) => {
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
-    req.user = user; // Ensure user is set here
+    req.user = user;
     next();
   });
 };
@@ -76,58 +71,46 @@ app.post("/auth/login", async (req, res) => {
 
   try {
     const user = await User.findOne({ username, role });
-    console.log(`${user}`);
-    console.log("dddd");
     if (user) {
       const isMatch = await bcrypt.compare(password, user.password);
-      console.log(isMatch);
       if (isMatch) {
-        console.log("i run to ffenearate token");
         const token = jwt.sign({ id: user._id, username: user.username }, process.env.JWT_SECRET, {
           expiresIn: 86400, // 24 hours
         });
-         res.status(200).json({ message: "Login successful", token, user });
-        } else {
-          res.status(401).json({ message: "Invalid credentials" });
-        }
+        res.status(200).json({ message: "Login successful", token, user });
       } else {
-        res.status(401).json({ doctormessage: "Invalid credentials" });
+        res.status(401).json({ message: "Invalid credentials" });
       }
-    } catch (error) {
-    console.log("i failed to ffenearate token");
-    res
-      .status(500)
-      .json({ message: "Error logging in user", error: error.message });
+    } else {
+      res.status(401).json({ message: "Invalid credentials" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Error logging in user", error: error.message });
   }
 });
-
 
 // Endpoint to register a new user
 app.post("/auth/register", async (req, res) => {
   const { username, password, role } = req.body;
   try {
-        const salt = await bcrypt.genSalt(10);
+    const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
     const newUser = new User({ username, password: hashedPassword, role });
     await newUser.save();
     res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
-    res
-      .status(400)
-      .json({ message: "Error registering user", error: error.message });
+    res.status(400).json({ message: "Error registering user", error: error.message });
   }
 });
 
 // Protected endpoint to get user details
 app.get("/auth/user/:username", authenticateToken, async (req, res) => {
   const { username } = req.params;
-  const { user } = req; // The user object from the token
+  const { user } = req;
 
   if (user.username !== username) {
-    return res
-      .status(403)
-      .json({ message: "Forbidden: You cannot access this user" });
+    return res.status(403).json({ message: "Forbidden: You cannot access this user" });
   }
   try {
     const user = await User.findOne({ username });
@@ -137,13 +120,9 @@ app.get("/auth/user/:username", authenticateToken, async (req, res) => {
       res.status(404).json({ message: "User not found" });
     }
   } catch (error) {
-    res.status(500).json({
-      message: "Error fetching user details server",
-      error: error.message,
-    });
+    res.status(500).json({ message: "Error fetching user details", error: error.message });
   }
 });
-
 
 // Endpoint to add a patient
 app.post('/api/patients', async (req, res) => {
@@ -152,10 +131,7 @@ app.post('/api/patients', async (req, res) => {
   try {
     const newPatient = new Patient({ name, age, gender, address, symptoms });
     await newPatient.save();
-    res.status(201).json({ 
-      message: 'Patient added successfully',
-      patient: newPatient
-    });
+    res.status(201).json({ message: 'Patient added successfully', patient: newPatient });
   } catch (error) {
     res.status(400).json({ message: 'Error adding patient', error: error.message });
   }
@@ -164,16 +140,12 @@ app.post('/api/patients', async (req, res) => {
 // Endpoint to fetch patient details by ID
 app.get('/api/patients/:patientId', async (req, res) => {
   const { patientId } = req.params;
-    console.log(patientId);
 
   try {
-    const patient = await Patient.findOne({patientId});
-    console.log(`${patient}`);
+    const patient = await Patient.findOne({ patientId });
     if (!patient) {
       return res.status(404).json({ message: 'Patient not found' });
     }
-    console.log("found");
-
     res.status(200).json({ patient });
   } catch (error) {
     res.status(500).json({ message: 'Error fetching patient details', error: error.message });
@@ -184,31 +156,24 @@ app.get('/api/patients/:patientId', async (req, res) => {
 app.post('/api/patients/:patientId/prescription', authenticateToken, async (req, res) => {
   const { patientId } = req.params;
   const { prescription } = req.body;
-  const { user } = req; // The user object from the token
+  const { user } = req;
 
   try {
-    // Check if the user making the request is authenticated and has a doctor role
     if (user.username !== "doctor") {
       return res.status(403).json({ message: "Unauthorized" });
     }
 
-    // Update the patient record with the new prescription
     const patient = await Patient.findOne({ patientId });
     if (!patient) {
       return res.status(404).json({ message: "Patient not found" });
     }
 
-    // Add the prescription to the patient's prescriptions array
     patient.prescriptions.push(prescription);
     await patient.save();
 
-    res
-      .status(200)
-      .json({ message: "Prescription added successfully", patient });
+    res.status(200).json({ message: "Prescription added successfully", patient });
   } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Error adding prescription", error: error.message });
+    res.status(500).json({ message: "Error adding prescription", error: error.message });
   }
 });
 
